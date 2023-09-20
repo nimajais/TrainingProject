2b62fc3cf6f25d34c7a50e468ba331abd1617af2
Arunima Jaiswal <nimajais@gmail.com>
2023-09-20 10:51:52 -0500

    Completed Image Displaying training project
    Completed training project using relevant documentation to display an image into the navigator.

--- NavigatorImageDisp.m
+++ NavigatorImageDisp.m
@@ -1,23 +1,29 @@
 % Arunima Jaiswal 
 % 9/20/23 Training Project 
 
+% Creates a function to display a custom image in the Navigator. 
 function NavigatorImageDisp
+    % Creates a new navigator with 24 plates. 
     n = navigator();
     plate = spcore.Plate.getDefault('PlateType', 'P24-1.5H-N'); 
     n.new(plate); 
     w = n.RootObject.getChildren('w', 1); 
+    % Creates a channel for a monochromatic image displaying in white. 
     monoChannel = spcore.ui.navigator.Channel('Name', 'Image', ..._ 
-        'Color', 'blue', ...
+        'Color', 'white', ...
         'CLim', [0 255], ...
         'CRange', [0, 255]); 
+    % Scales the plate and transforms the image to fit properly. 
     plateScale = [sign(0.5 - plate.XReverse), -sign(0.5 - plate.YReverse)];
     T = spcore.ui.navigator.Image.getTransformation(...
-    'Scale', plateScale.*[2, 2], ...
-    'Translate', [w.XPosition + 1.5, w.YPosition]);
+    'Scale', plateScale.*[19, 19], ...
+    'Translate', [w.XPosition, w.YPosition + 1]);
+    % Adds the custom image onto the plate (MATLAB default image used) 
     mMono = monoChannel.addImage(...
-    'CData', imread('MicroscopeNeuron.tif'), ...
+    'CData', imread('mri.tif'), ...
     'Transformation', T);
+    % Updates the navigator.  
     n.addChannel('Channel', monoChannel);
     n.CurrentObject = w;
-    n.zoomFit('selected');
+    %n.zoomFit('selected'); % Zooms onto plate with the image. 
 
