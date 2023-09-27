00058025c21c94bf0ea8eef961820060ad0c69cd
Arunima Jaiswal <nimajais@gmail.com>
2023-09-27 11:12:42 -0500

    Attempts to scale and transform image.

--- NavigatorImageDisp.m
+++ NavigatorImageDisp.m
@@ -2,14 +2,15 @@
 % 9/20/23 Training Project 
 
 % Creates a function to display a custom image in the Navigator. 
-function NavigatorImageDisp
+function NavigatorImageDisp(xpos, ypos)
     % Creates a new navigator with 24 plates. 
     n = navigator();
+    n.DarkMode = false;
     plate = spcore.Plate.getDefault('PlateType', 'P24-1.5H-N'); 
     n.new(plate); 
     % Provides the image info for the sample image. 
-    imgarray = imfinfo('mri.tif'); 
-    disp(imgarray(6)) 
+    imgarray = imfinfo('forest.tif'); 
+    disp(imgarray(1)) 
     % Asks the user for which well they'd like ot use to display their
     % image, and displays it in that well accordingly. 
     wellprompt = "Which well (1-24) would you like to display your image in?"; 
@@ -26,18 +27,24 @@ function NavigatorImageDisp
     % and height of the image. 
     xscaleprompt = "What is the width of your image?"; 
     xscale = input(xscaleprompt); 
-    xscale = 2432 / xscale; 
     yscaleprompt = "What is the height of your image?"; 
     yscale = input(yscaleprompt); 
-    yscale = 2432 / yscale;  
+    yscale = 2432 / (sqrt((0.5*xscale^2)+(0.5*yscale^2)));   
+    xscale = yscale;  
+    % Vertically and horizontally moves the image according to its position
+    % in the well. 
+    xpos = w.XPosition - xpos; 
+    ypos = w.YPosition - ypos; 
+    %disp(w.XPosition + ", " + w.YPosition 
     T = spcore.ui.navigator.Image.getTransformation(...
     'Scale', plateScale .* [xscale, yscale], ...
-    'Translate', [w.XPosition, w.YPosition + 1]);
+    'Translate', [xpos, ypos]);
     % Adds the custom image onto the plate (MATLAB default image used) 
     mMono = monoChannel.addImage(...
-    'CData', imread('mri.tif'), ...
+    'CData', imread('forest.tif'), ...
     'Transformation', T);
-    % Updates the navigator.  
+    % Updates the navigator. 
     n.addChannel('Channel', monoChannel);
     n.CurrentObject = w;
     n.zoomFit('selected'); % Zooms onto plate with the image. 
+
